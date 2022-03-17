dd564eb44b12f8bdbebeb63dc7ec5aad47e3860a
ganchana youpaisan <ganchana78@gmail.com>
2022-03-17 20:27:34 +0700

    full version

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,7 +34,7 @@ function App() {
     setPlaylistName(name)
   }
 
-  const savePlaylist = () => {
+  const savePlaylist =  () => {
     const trackUris = playlistTracks.map(track => track.uri);
     Spotify.savePlaylist(playlistName, trackUris).then(() => {
       setPlaylistName('')
